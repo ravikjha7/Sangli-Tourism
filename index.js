36aa2603be0ae702c5080750ed273d238aee64d5
ravikjha7 <ravikjha7@gmail.com>
2023-02-08 12:03:03 +0530

    Cors

--- index.js
+++ index.js
@@ -8,10 +8,15 @@ const port = process.env.PORT || 3000;
 const userRoutes = require('./routes/users');
 const adminRoutes = require('./routes/admins');
 
-app.use(bodyParser.json({ limit: "30mb", extended: true}));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+
+const corsOptions = {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+};
+app.use(cors(corsOptions));
 
 connectDB();
 
