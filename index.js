005ce4b55bf111d412abb40206c13b68e6bc6315
ravikjha7 <ravikjha7@gmail.com>
2023-02-08 12:36:17 +0530

    Add

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 const userRoutes = require('./routes/users');
 const adminRoutes = require('./routes/admins');
 
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.json({ limit: "30mb", extended: true })); 
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ extended: true }));
 
