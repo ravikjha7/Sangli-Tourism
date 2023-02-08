468af9d62293c6314fadfb40c73c0247ab09f2d7
ravikjha7 <ravikjha7@gmail.com>
2023-02-08 12:56:39 +0530

    Removed CORS Options

--- index.js
+++ index.js
@@ -12,7 +12,12 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors());
+var corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200,
+  }
+
+app.use(cors(corsOptions));
 
 connectDB();
 
